04efc94721bedcfdac21329fb1dfc887300b8eaf
THOUFEEK-MI <ibrahimmadathiparambil99@gmail.com>
2026-06-08 08:09:12 +0530

    Add CSRF trusted origins

--- invoice_project/settings.py
+++ invoice_project/settings.py
@@ -28,7 +28,9 @@ DEBUG = False
 ALLOWED_HOSTS = [ "invoice-app-520919893407.asia-south1.run.app",
     "localhost",
     "127.0.0.1",]
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://invoice-app-520919893407.asia-south1.run.app",
+]
 
 # Application definition
 
--- settings.py
+++ settings.py
@@ -1,4 +1,14 @@
 LOGIN_URL = "/admin/login/"
 TIME_ZONE = 'Asia/Kolkata'
 USE_TZ = True
+DEBUG = False
 
+ALLOWED_HOSTS = [
+    "invoice-app-520919893407.asia-south1.run.app",
+    "localhost",
+    "127.0.0.1",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://invoice-app-520919893407.asia-south1.run.app",
+]
